ec1a3d4134d0a8c4972894a6fb5d9033d8ef2571
djeongo <ubuntu@ip-172-31-85-139.ec2.internal>
2020-11-02 05:29:45 +0000

    Update VIRAT tools

--- mytools/split_virat_v2.py
+++ mytools/split_virat_v2.py
@@ -4,12 +4,11 @@ import os.path
 import glob
 import pandas as pd
 import re
-from virat_utils import parse_video_name
+import virat_utils
 from sklearn.model_selection import train_test_split
 
 MAX_VIDEOS = -1
-FPS=30
-ANNOTATION_FILES="/data/virat-v2/raw-annotations/*events.txt" # file containing event id and bounding box   
+ANNOTATION_FILES="/data/virat-v2/raw-annotations/*events.txt" # file containing event id and bounding box
 VIDEO_FRAMES_PATH="/data/virat-v2/frames/" # video frames path
 FRAME_LISTS_PATH="/data/virat-v2/frame_lists/"
 ANNOTATION_OUTPUT_PATH="/data/virat-v2/annotations/"
@@ -38,7 +37,7 @@ if False:
     dfs = []
     for path in glob.glob(ANNOTATION_FILES):
         video = os.path.basename(path.split('.')[0])
-        collection_group_id, scene_id = parse_video_name(video)
+        collection_group_id, scene_id = virat_utils.parse_video_name(video)
         annotations = process_events_file(path)
         df = pd.DataFrame(annotations)
         df.loc[:,'video'] = video
@@ -72,33 +71,33 @@ def validate_frames(dfs):
         exists = os.path.exists(video_path)
         if not exists:
             print(video_path, exists)
-        
+
         start_frame = row['start_frame']
         end_frame = row['end_frame']
         start_frame_path = '{}/{}_{:06d}.jpg'.format(video_path, row['video'], start_frame)
         exists = os.path.exists(start_frame_path)
         if not exists:
             print('start_frame:',start_frame_path, exists)
-        
+
         end_frame_path = '{}/{}_{:06d}.jpg'.format(video_path, row['video'], end_frame)
         exists = os.path.exists(end_frame_path)
         if not exists:
             print('end_frame:',end_frame_path, exists)
 
 
-def format_for_slowfast(dfs):
-    dfs_slowfast = []
-    for video_id, df in enumerate(dfs):
-        # print('{}/{} formatting for slowfast'.format(video_id, len(dfs)))
-        row = df.iloc[0]
-        video_path = VIDEO_FRAMES_PATH + row['video']
-        df.loc[:,'original_vido_id'] = df['video']
-        df.loc[:,'video_id'] = video_id
-        df.loc[:,'frame_id'] = df['current_frame']-df['start_frame']
-        df.loc[:,'path'] = df.apply(lambda row: '{}/{}_{:06d}.jpg'.format(video_path, row['video'], row['current_frame']+1), axis=1)       
-        df.loc[:,'labels'] = df.apply(lambda row: '"{}"'.format(row['event_type']-1), axis=1)
-        dfs_slowfast.append(df[['original_vido_id', 'video_id', 'frame_id', 'path', 'labels']])
-    return dfs_slowfast
+# def format_for_slowfast(dfs):
+#     dfs_slowfast = []
+#     for video_id, df in enumerate(dfs):
+#         # print('{}/{} formatting for slowfast'.format(video_id, len(dfs)))
+#         row = df.iloc[0]
+#         video_path = VIDEO_FRAMES_PATH + row['video']
+#         df.loc[:,'original_vido_id'] = df['video']
+#         df.loc[:,'video_id'] = video_id
+#         df.loc[:,'frame_id'] = df['current_frame']-df['start_frame']
+#         df.loc[:,'path'] = df.apply(lambda row: '{}/{}_{:06d}.jpg'.format(video_path, row['video'], row['current_frame']+1), axis=1)
+#         df.loc[:,'labels'] = df.apply(lambda row: '"{}"'.format(row['event_type']-1), axis=1)
+#         dfs_slowfast.append(df[['original_vido_id', 'video_id', 'frame_id', 'path', 'labels']])
+#     return dfs_slowfast
 
 VIDEO_IDS = {} # video->video_id
 
@@ -130,12 +129,6 @@ def generate_frame_lists_files(videos):
         print("There are {} empty dirs!".format(empty_dirs))
     return dfs_frames
 
-def get_resolution(video):
-    return 1,1
-    img = cv2.imread(frame_path)
-    height, width, channels = img.shape
-    return width, height
-
 def generate_annotation_files_v2():
     dfs_annotations = []
     for video_id, event_file in enumerate(glob.glob(ANNOTATION_FILES)):
@@ -148,28 +141,38 @@ def generate_annotation_files_v2():
         to_slowfast(video, df)
 
         dfs_annotations.append(df[['video_name', 'frame_sec', 'x1', 'y1', 'x2', 'y2', 'label', 'score']])
-        
+
+        if MAX_VIDEOS > 0 and video_id+1 >= MAX_VIDEOS: break
+
     print('len(dfs_annotations): {}'.format(len(dfs_annotations)))
     return dfs_annotations
 
 def to_slowfast(video, df):
-    width, height = get_resolution(video)
+    orig_width, orig_height = virat_utils.get_original_resolution(video)
+    cur_width, cur_height = virat_utils.get_frame_resolution(video)
+    width_ratio = orig_width / cur_width
+    height_ratio = orig_height / cur_height
+    x1 = df['lefttop_x'] / width_ratio
+    y1 = df['lefttop_y'] / height_ratio
+    x2 = (df['lefttop_x'] + df['width']) / width_ratio
+    y2 = (df['lefttop_y'] + df['height']) / height_ratio
+    fps = virat_utils.get_fps(video)
     df.loc[:,'video_name'] = video
-    df.loc[:,'frame_sec'] = df['current_frame'] // FPS
-    df.loc[:,'x1'] = df['lefttop_x'] / width
-    df.loc[:,'y1'] = df['lefttop_y'] / height
-    df.loc[:,'x2'] = (df['lefttop_x'] + df['width']) / width
-    df.loc[:,'y2'] = (df['lefttop_y'] + df['height']) / height
-    df.loc[:,'label'] = df['event_type']-1
+    df.loc[:,'frame_sec'] = (df['current_frame'] // fps).astype(int)
+    df.loc[:,'x1'] = x1 / cur_width
+    df.loc[:,'y1'] = y1 / cur_height
+    df.loc[:,'x2'] = x2 / cur_width
+    df.loc[:,'y2'] = y2 / cur_height
+    df.loc[:,'label'] = df['event_type']
     df.loc[:,'score'] = 1
 
 # def generate_annotation_files(dfs, dfs_slowfast):
-#     # Process 
+#     # Process
 #     #   *events*.csv files
 #     # we want output to be:
 #     #   video_id, frame_sec, x1, y1, x2, y2, label, score
 #     #   * Box with format [x1, y1, x2, y2] with a range of [0, 1] as float.
-#     #   * score doesn't matter 
+#     #   * score doesn't matter
 #     # VIRAT bounding box format: 'lefttop_x', 'lefttop_y', 'width', 'height'
 #     #
 #     # Input
@@ -217,9 +220,9 @@ for split in dfs_annotations_map.keys():
     dfs = dfs_annotations_map[split]['dfs']
     df = pd.concat(dfs)
     print(f"Spliit: {split}, len: {len(dfs)}")
-    train_annotation_path = f'{ANNOTATION_OUTPUT_PATH}/{dfs_annotations_map[split]["csv_annotations"]}'
-    print("Writing "+train_annotation_path)
-    df.to_csv(train_annotation_path, sep=',', index=False, quoting=csv.QUOTE_NONE, header=False)
+    annotation_path = f'{ANNOTATION_OUTPUT_PATH}/{dfs_annotations_map[split]["csv_annotations"]}'
+    print("Writing "+annotation_path)
+    df.drop_duplicates().to_csv(annotation_path, sep=',', index=False, quoting=csv.QUOTE_NONE, header=False)
 
 print('len(VIDEO_IDS): {}'.format(len(VIDEO_IDS)))
 for split in dfs_annotations_map.keys():
@@ -238,7 +241,7 @@ for split in dfs_annotations_map.keys():
 
 # train_csv_path = f'{OUTPUT_PATH}/train.csv'
 # val_csv_path = f'{OUTPUT_PATH}/val.csv'
-# train_annotation_path = f'{ANNOTATION_OUTPUT_PATH}/train_annotation.csv'
+# annotation_path = f'{ANNOTATION_OUTPUT_PATH}/train_annotation.csv'
 # val_annotation_path = f'{ANNOTATION_OUTPUT_PATH}/val_annotation.csv'
 
 # print("Writing "+train_csv_path)
@@ -247,8 +250,8 @@ for split in dfs_annotations_map.keys():
 # print("Writing "+val_csv_path)
 # pd.concat(dfs_slowfast_dev).to_csv(val_csv_path, sep=' ', index=False, quoting=csv.QUOTE_NONE)
 
-# print("Writing "+train_annotation_path)
-# pd.concat(df_annotations_train).to_csv(train_annotation_path, sep=',', index=False, quoting=csv.QUOTE_NONE, header=False)
+# print("Writing "+annotation_path)
+# pd.concat(df_annotations_train).to_csv(annotation_path, sep=',', index=False, quoting=csv.QUOTE_NONE, header=False)
 
 # print("Writing "+val_annotation_path)
 # pd.concat(df_annotations_dev).to_csv(val_annotation_path, sep=',', index=False, quoting=csv.QUOTE_NONE, header=False)
